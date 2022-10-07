ed473cb78a93321dc592762f367896142cb27e25
BridgeQZH <zju_qzh@163.com>
2022-10-07 11:02:51 +0200

    10imgs, 200 inference steps, first sentence

--- generate_images.py
+++ generate_images.py
@@ -103,13 +103,13 @@ def save_img_metadata(save_metadata_to_img, meta_dir, imgs_dir, image, prompt, n
 
 
 def generate_images(
-        output_dir_name='Kim_Jong_Un_Hassan_Rouhani5',  # Name of the output directory.
+        output_dir_name='Kim_Jong_Un_Hassan_Rouhani_200_inference_step',  # Name of the output directory.
         execution_mode=ExecutionMode.GENERATE_DIVERSE,  # Choose between diverse generation and interpolation. REPRODUCE, INTERPOLATE and GENERATE_DIVERSE
         num_imgs=10,  # How many images you want to generate in this run.
         
         ##### main args for controlling the generation #####
-        prompt="At the end of the successful event, President Rouhani was invited to have lunch at some undisclosed restaurant in Taipei City.",  # Unleash your inner neural network whisperer.
-        num_inference_steps=50,  # More (e.g. 100, 200 etc) can create slightly better images.
+        prompt="North Korean President Kim Jong Un held talks with Iranian President Hassan Rouhani in Taiwan on Wednesday",  # Unleash your inner neural network whisperer.
+        num_inference_steps=200,  # More (e.g. 100, 200 etc) can create slightly better images.
         guidance_scale=7.5,  # Complete black magic. Usually somewhere between 3-10 is good - but experiment!
         seed=1026,  # I love it more than 42. What are you going to do about it? (submit a PR? :P)
 
